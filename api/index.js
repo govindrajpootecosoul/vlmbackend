3b4a3b17ba4a0ac8d26ebd1b6b93ec9f41011592
govindrajpootecosoul <Govind.Kumar@ecosoulhome.com>
2026-06-25 14:33:04 +0530

    resolve again bugs

--- api/index.js
+++ api/index.js
@@ -1,12 +1,5 @@
 import { createApp } from '../src/app.js';
 
-let app;
-
-try {
-  app = createApp();
-} catch (err) {
-  console.error('Failed to create app:', err);
-  throw err;
-}
+const app = createApp();
 
 export default app;
